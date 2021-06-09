040ef62c39369db5f444955de60b0b93ddfa0a1a
Jannik Behrens <56647862+JannikBehrens@users.noreply.github.com>
2021-06-09 13:18:38 +0200

    Validation with jest
    Added test folder and installed jest. Command npm run test starts the test

---
+++ projects/evaluate-news-nlp/__test__/nameChecker.test.js
@@ -0,0 +1,25 @@
+
+import { validateUrl } from "../src/client/js/nameChecker.js";
+
+describe("Correct URLs", () => {
+  test("should return true (https)", () => {
+    const input = "https://www.udacity.com";
+    expect(validateUrl(input)).toBe(true);
+  });
+  test("should return true (http)", () => {
+    const input = "http://www.udacity.com";
+    expect(validateUrl(input)).toBe(true);
+  });
+});
+
+describe("No http(s)/no www", () => {
+  test("should return false (no http)", () => {
+    const input = "www.udacity.com";
+    expect(validateUrl(input)).toBe(false);
+  });
+  test("should return false (no www)", () => {
+    const input = "udacity.com";
+    expect(validateUrl(input)).toBe(false);
+  });
+  
+});
